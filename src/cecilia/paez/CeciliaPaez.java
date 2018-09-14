becb00102e710c7a0be1b40ac026f59f858a3346
T-101 <T-101@PC214>
2018-09-13 19:44:58 -0500

    Mi primer proyecto de Github

---
+++ src/cecilia/paez/CeciliaPaez.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cecilia.paez;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author T-101
+ */
+public class CeciliaPaez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //System.out.print("Hola mundo");
+        JOptionPane.showConfirmDialog(null, "Hola mundo");
+    }
+    
+}
